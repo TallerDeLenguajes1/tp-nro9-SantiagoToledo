29871eb0a6cf8c7b1879dcf493c8ee7c4baef67f
Santiago <santi.nt2@gmail.com>
2019-05-30 19:40:10 -0300

    Creer funcion para mover archivo

---
+++ TP9/TP9/Program.cs
@@ -0,0 +1,37 @@
+﻿using Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoverArchivos();
+
+        }
+
+        public static void MoverArchivos(){
+
+            string[] archivos = Directory.GetFiles(Directory.GetCurrentDirectory());
+            string destino = SoporteParaConfiguracion.LeerConfiguracion();
+         
+            foreach (string archivo in archivos)
+            {
+                string extension = Path.GetExtension(archivo);
+                if (extension == ".txt" || extension == ".mp3")
+                {
+                    File.Copy(archivo, destino + Path.GetFileName(archivo), true);
+                    Console.WriteLine("Se copio {0}", Path.GetFileName(archivo));
+                }
+           
+            }
+        }
+
+    }
+}
---
+++ TP9/TP9/SoporteParaConfiguracion.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    public static class SoporteParaConfiguracion
+    {
+        static string NombreArchivo = "configuracion.dat";
+        static string RutaArchivo = @"C:\ArchivosTP9\";
+
+        public static void CrearArchivoDeConfiguracion()
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open(RutaArchivo + NombreArchivo, FileMode.Create)))
+            {
+                writer.Write(RutaArchivo);
+            }
+
+        }
+
+
+        public static string LeerConfiguracion()
+        {
+
+            if (!File.Exists(NombreArchivo)) {
+                CrearArchivoDeConfiguracion();
+            }
+            using (BinaryReader reader = new BinaryReader(File.Open(RutaArchivo + NombreArchivo, FileMode.Open)))
+            {
+                string ruta = reader.ReadString();
+                return ruta;
+            }
+           
+        }
+    }
+
+    public static class ConversorDeMorse
+    {
+        private static readonly Dictionary<string, string> LetraMorse = new Dictionary<string, string>
+        {
+            { "a", ".-" },
+            { "b", "-..." },
+
+        };
+
+
+        public static string MorseATexto(string morse)
+        {
+
+        }
+        public static string TextoAMorse(string texto)
+        {
+
+        }
+    }
+}
